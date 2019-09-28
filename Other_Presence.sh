a3fb136093d1f5f8762ada0c4fe2aeabf862c6f6
sasial-roblox <44125644+sasial-roblox@users.noreply.github.com>
2019-09-29 07:54:29 +1000

    Remove Shell commands

--- Other_Presence.sh
+++
@@ -1,6 +0,0 @@
-echo ------
-echo Please do not close this window.
-echo ------
-echo If you have not done the installion (other_install.sh), please do that first or else an error will occur.
-echo ------
-npm start
--- Windows_Install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ------
-echo Please wait for the installer to finish installing.
-echo It may take a while.
-echo ------
-echo Your console might say > node scripts/install.js
-echo That is all normal, you will just have to wait.
-echo ------
-npm install
